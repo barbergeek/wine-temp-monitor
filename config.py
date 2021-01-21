26c19b40bcb34bf2587aeac5fa71d933d233d835
Scott Hoge <scott.hoge@gmail.com>
2021-01-20 22:20:43 -0500

    config file

---
+++ config.py
@@ -0,0 +1,2 @@
+MQTT_TOPIC = "home/basement/wine/temperature"
+BROKER_ADDR = "192.168.16.10"
--- temperature.py
+++ temperature.py
@@ -1,9 +1,10 @@
 import os
 import glob
 import time
-#from ISStreamer.Streamer import Streamer
+from datetime import datetime
+import paho.mqtt.client as mqtt
 
-#streamer = Streamer(bucket_name="Temperature Stream", bucket_key="piot_temp_stream031815", access_key="PUT_YOUR_ACCESS_KEY_HERE")
+from config import BROKER_ADDR, MQTT_TOPIC
 
 os.system('modprobe w1-gpio')
 os.system('modprobe w1-therm')
@@ -11,6 +12,8 @@ os.system('modprobe w1-therm')
 base_dir = '/sys/bus/w1/devices/'
 device_folder = glob.glob(base_dir + '28*')[0]
 device_file = device_folder + '/w1_slave'
+
+mqtt_client = mqtt.Client("wine-temp")
 
 def read_temp_raw():
     f = open(device_file, 'r')
@@ -32,10 +35,11 @@ def read_temp():
 
 while True:
     temp_c = read_temp()
-    temp_f = temp_c * 9.0 / 5.0 + 32.0
-    #streamer.log("temperature (C)", temp_c)
-    #streamer.log("temperature (F)", temp_f)
-    print("temperature (C)", temp_c)
-    print("temperature (F)", temp_f)
-    time.sleep(.5)
+    temp_f = round(temp_c * 9.0 / 5.0 + 32.0,2)
+    mqtt_client.connect(BROKER_ADDR)
+    mqtt_client.publish(MQTT_TOPIC, temp_f)
+    mqtt_client.disconnect()
+    now = datetime.now()
+    print(now.strftime("%d/%m/%Y %H:%M:%S"),'temperature (F)', temp_f)
+    time.sleep(60)
 
